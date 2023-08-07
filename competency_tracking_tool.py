c6da0534967b24883423125e4d4b1a33536a5c1a
Seth Bauer <sethhbauer@gmail.com>
2023-08-07 16:03:53 -0600

    Git Initialization

---
+++ competency_tracking_tool.py
@@ -0,0 +1,38 @@
+
+
+class Manager():
+    # can view all users
+    # search for users by first or last name
+    # view reports of users grouped by competency
+    # view comptency of individual user
+    # view assessments for a user
+    # add: user, competency, assessment to competency, assessment result
+    # edit: user info, competency, assessment, assessment result
+    # delete: assessment result
+    pass
+
+
+class User():
+    # can view their own data but no one else's
+    # can change certain elements of their data, such as name, password, or email
+    pass
+
+
+def export_data():
+    # comptetency reports
+    # single user competency
+    # export as csv and pdf
+    pass
+
+
+def import_data():
+    # assessment results
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
