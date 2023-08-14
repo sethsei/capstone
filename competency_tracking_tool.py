d0203f31b577d7ccda20918f3730b02e80307bb7
Seth Bauer <sethhbauer@gmail.com>
2023-08-14 16:44:21 -0600

    Users can now view and change their own account information

--- competency_tracking_tool.py
+++ competency_tracking_tool.py
@@ -1,7 +1,8 @@
-import sqlite3, bcrypt, getch, csv, sys
+import sqlite3, bcrypt, getch, csv, sys, curses
 from datetime import date
 from os import system, path
 from termcolor import cprint, colored
+from curses import wrapper
 
 
 class User():
@@ -46,7 +47,7 @@ class User():
     def _create_password(self):
         while True:
             clear()
-            pw = getpass('\n\nInput Password:  ')
+            pw = getpass('\n\nInput Password:    ')
             confirm = getpass('\n\nConfirm Password:  ')
 
             pw = pw.encode()
@@ -85,19 +86,192 @@ class User():
         connection.commit()
 
 
+    def change_first_name(self):
+        while True:
+            clear()
+            print('\n\nInput New First Name:  ', end='')
+            new_first_name = input()
+
+            if new_first_name == self.first_name:
+                print('\n\nThis is already your first name. Would you like to cancel this change?  Y/N\n')
+                yn = input().upper()
+                
+                if yn == 'Y':
+                    cprint('\n\n--- Change Canceled ---', 'light_yellow', attrs=['bold'])
+                    wait_for_keypress()
+                    return
+                
+                elif yn == 'N':
+                    continue
+
+                else:
+                    cprint('\n\nInvalid input. Try again.', 'red')
+                    wait_for_keypress()
+                    continue
+            
+            self.first_name = new_first_name
+            self._update_database('First Name', self.first_name)
+
+            cprint('\n\n--- First Name Updated ---', 'light_green', attrs=['bold'])
+            wait_for_keypress()
+            return
+
+
+    def change_last_name(self):
+        while True:
+            clear()
+            print('\n\nInput New Last Name:  ', end='')
+            new_last_name = input()
+
+            if new_last_name == self.last_name:
+                print('\n\nThis is already your last name. Would you like to cancel this change?  Y/N\n')
+                yn = input().upper()
+                
+                if yn == 'Y':
+                    cprint('\n\n--- Change Canceled ---', 'light_yellow', attrs=['bold'])
+                    wait_for_keypress()
+                    return
+                
+                elif yn == 'N':
+                    continue
+
+                else:
+                    cprint('\n\nInvalid input. Try again.', 'red')
+                    wait_for_keypress()
+                    continue
+            
+            self.last_name = new_last_name
+            self._update_database('Last Name', self.last_name)
+
+            cprint('\n\n--- Last Name Updated ---', 'light_green', attrs=['bold'])
+            wait_for_keypress()
+            return
+
+
+    def change_phone(self):
+        while True:
+            clear()
+            print('\n\nInput New Phone Number:    ', end='')
+            new_phone = input()
+
+            if new_phone == self.phone:
+                print('\n\nThis is already your phone number. Would you like to cancel this change?  Y/N\n')
+                yn = input().upper()
+                
+                if yn == 'Y':
+                    cprint('\n\n--- Change Canceled ---', 'light_yellow', attrs=['bold'])
+                    wait_for_keypress()
+                    return
+                
+                elif yn == 'N':
+                    continue
+
+                else:
+                    cprint('\n\nInvalid input. Try again.', 'red')
+                    wait_for_keypress()
+                    continue
+            
+            elif new_phone.isnumeric() == False:
+                cprint('\n\nInvalid input. Must be a real phone number consisting of only digits 0-9', 'red')
+                wait_for_keypress()
+                continue
+
+            else:
+                print('\nConfirm New Phone Number:  ', end='')
+                con_phone = input()
+
+                if new_phone == con_phone:
+                    self.phone = new_phone
+                    self._update_database('Phone', self.phone)
+                    cprint('\n\n--- Phone Number Updated ---', 'light_green', attrs=['bold'])
+                    wait_for_keypress()
+                    return
+                
+                else:
+                    cprint('\n\nNumbers do not match', 'red')
+                    wait_for_keypress()
+                    continue
+
+
+    def change_email(self):
+        while True:
+            clear()
+            print('\n\nInput New Email:    ', end='')
+            new_email = input()
+
+            if new_email == self.email:
+                print('\n\nThis is already your email. Would you like to cancel this change?  Y/N\n')
+                yn = input().upper()
+                
+                if yn == 'Y':
+                    cprint('\n\n--- Change Canceled ---', 'light_yellow', attrs=['bold'])
+                    wait_for_keypress()
+                    return
+                
+                elif yn == 'N':
+                    continue
+
+                else:
+                    cprint('\n\nInvalid input. Try again.', 'red')
+                    wait_for_keypress()
+                    continue
+
+            query = find_query('Login')
+            row = cursor.execute(query, (new_email,)).fetchone()
+
+            if row:
+                cprint('\n\nThis email is already in use. Please choose a different one.', 'red')
+                wait_for_keypress()
+                continue
+
+            else:
+                print('\nConfirm New Email:  ', end='')
+                con_email = input()
+
+                if new_email == con_email:
+                    self.email = new_email
+                    self._update_database('Email', self.email)
+                    cprint('\n\n--- Email Updated ---', 'light_green', attrs=['bold'])
+                    wait_for_keypress()
+                    return
+                
+                else:
+                    cprint('\n\nEmails do not match', 'red')
+                    wait_for_keypress()
+                    continue
+    
+
     def change_password(self):
         clear()
         pw = getpass('\n\nInput Old Password:  ').encode()
 
         if not bcrypt.checkpw(pw, self.password.encode()):
-            cprint('\n\nIncorrect Password\n\n', 'red')
+            cprint('\n\nIncorrect Password', 'red')
             wait_for_keypress()
             clear()
             return
 
         while True:
             clear()
-            new_password = getpass('\n\nInput New Password:  ').encode()
+            new_password = getpass('\n\nInput New Password:    ').encode()
+
+            if bcrypt.checkpw(new_password, self.password.encode()):
+                print('\n\nThis is already your password. Would you like to cancel this change?  Y/N\n')
+                yn = input().upper()
+                
+                if yn == 'Y':
+                    cprint('\n\n--- Change Canceled ---', 'light_yellow', attrs=['bold'])
+                    wait_for_keypress()
+                    return
+                
+                elif yn == 'N':
+                    continue
+
+                else:
+                    cprint('\n\nInvalid input. Try again.', 'red')
+                    wait_for_keypress()
+                    continue
+
             confirm_new_password = getpass('\nConfirm New Password:  ').encode()
 
             if new_password == confirm_new_password:
@@ -116,18 +290,6 @@ class User():
                 cprint('\n\nPasswords do not match. Try again.', 'red')
                 wait_for_keypress()
                 continue
-    
-
-    def change_email(self):
-        print('\n\nInput New Email:  ', end='')
-        self.email = input()
-        clear()
-
-        cprint('\n\n--- Email Changed ---', 'light_green', attrs=['bold'])
-        wait_for_keypress()
-        clear()
-        self._update_database('Email', self.email)
-        return
 
 
     def print_info(self):
@@ -141,23 +303,31 @@ class User():
         else:
             phone_num = self.phone
 
-        uid = colored(f'{"User ID:":16}', 'white', attrs=['bold'])
-        fn = colored(f'{"First Name:":16}', 'white', attrs=['bold'])
-        ln = colored(f'{"Last Name:":16}', 'white', attrs=['bold'])
-        e = colored(f'{"Email:":16}', 'white', attrs=['bold'])
-        pa = colored(f'{"Password:":16}', 'white', attrs=['bold'])
-        ph = colored(f'{"Phone:":16}', 'white', attrs=['bold'])
-        dc = colored(f'{"Date Created:":16}', 'white', attrs=['bold'])
+        if self.user_type == 0:
+            user_type = 'User'
+        
+        else:
+            user_type = 'Manager'
 
-        cprint(f'\n\n{"User Info":^46}', 'white', attrs=['bold'])
-        cprint('-'*46, 'light_grey')
-        print(f'{uid}{self.user_id:>30}')
-        print(f'{fn}{self.first_name:>30}')
-        print(f'{ln}{self.last_name:>30}')
-        print(f'{e}{self.email:>30}')
-        print(f'{pa}{"*"*8:>30}')
-        print(f'{ph}{phone_num:>30}')
-        print(f'{dc}{self.date_created:>30}')
+        uid = colored(f'{"User ID:":20}', 'white', attrs=['bold'])
+        fn = colored(f'{"First Name:":20}', 'white', attrs=['bold'])
+        ln = colored(f'{"Last Name:":20}', 'white', attrs=['bold'])
+        ph = colored(f'{"Phone:":20}', 'white', attrs=['bold'])
+        e = colored(f'{"Email:":20}', 'white', attrs=['bold'])
+        dc = colored(f'{"Date Created:":20}', 'white', attrs=['bold'])
+        hd = colored(f'{"Hire Date:":20}', 'white', attrs=['bold'])
+        ut = colored(f'{"User Type:":20}', 'white', attrs=['bold'])
+
+        cprint(f'\n\n{"Account Information":^70}', 'white', attrs=['bold'])
+        cprint('-'*70, 'light_grey')
+        cprint(f'{uid}{self.user_id:>50}')
+        cprint(f'{fn}{self.first_name:>50}')
+        cprint(f'{ln}{self.last_name:>50}')
+        cprint(f'{ph}{phone_num:>50}')
+        cprint(f'{e}{self.email:>50}')
+        cprint(f'{dc}{self.date_created:>50}')
+        cprint(f'{hd}{self.hire_date:>50}')
+        cprint(f'{ut}{user_type:>50}')
 
         change_values()
 
@@ -287,16 +457,28 @@ def get_user():
         current_user.print_info()
 
 
+def change_values_menu():
+    e = colored('EXIT', 'light_blue', attrs=['bold'])
+    print(f'\n\n\n{"-"*70}\nIf you would like to change a detail please select an option below,\nor type <{e}> to return to the menu\n{"-"*70}\n')
+    print('    (F)irst Name')
+    print('    (L)ast Name')
+    print('    (PH)one Number')
+    print('    (E)mail')
+    print('    (PA)ssword')
+    print(f'\n{"-"*70}\nIf the value you would like to change is not listed above,\nplease contact your administrator to rectify the information.\n{"-"*70}\n\n')
+
+
 def change_values():
-    em = colored('E', 'light_green', attrs=['bold'])
-    p = colored('P', 'light_green', attrs=['bold'])
-    ex = colored('EXIT', 'light_blue', attrs=['bold'])
-    print(f'\n\nIf you would like to change the email or password type <{em}> or <{p}> respectively or <{ex}> to return to users:  ', end='')
+    global current_user
+    change_values_menu()
     user_input = input().upper()
     clear()
 
-    inputs = {'E': current_user.change_email,
-              'P': current_user.change_password}
+    inputs = {'F': current_user.change_first_name,
+              'L': current_user.change_last_name,
+              'PH': current_user.change_phone,
+              'E': current_user.change_email,
+              'PA': current_user.change_password}
 
     if user_input == 'EXIT':
         return
@@ -305,7 +487,8 @@ def change_values():
         inputs[user_input]()
 
     else:
-        cprint('\n\nInvalid Input', 'red')
+        cprint('\n\nInvalid Input. Try again.', 'red')
+        wait_for_keypress()
         return
 
 
@@ -326,13 +509,6 @@ def create_database():
 
     cursor.executescript(queries)
     connection.commit()
-
-
-def print_main_menu():
-    cprint(f'\n\n{"Menu":^16}', 'white', attrs=['bold'])
-    print('-'*16)
-    print('  (L)og In')
-    cprint('  (Q)uit\n\n', 'red')
 
 
 def getpass(prompt):
@@ -366,7 +542,7 @@ def login():
         query = find_query('Get User')
         row = cursor.execute(query, (row[0],)).fetchone()
 
-        password = getpass('\n\nEnter your password:  ').encode()
+        password = getpass('\nEnter your password:  ').encode()
         if not bcrypt.checkpw(password, row[5].encode()):
             cprint('\n\nIncorrect password.', 'red')
             wait_for_keypress()
@@ -377,11 +553,58 @@ def login():
             current_user = User(row[1], row[2], row[3], row[4], row[8], user_id=row[0], password=row[5], user_type=row[9])
 
             query = find_query('Update Status')
-            cursor.execute(query, (row[0],))
+            cursor.execute(query, (1, current_user.user_id))
             connection.commit()
             
-            cprint('\n\nSuccessfully logged in.', 'light_green', attrs=['bold'])
+            cprint('\n\n--- Successfully Logged In ---', 'light_green', attrs=['bold'])
+            cprint(f'\n{"Welcome " + current_user.first_name:^30}', 'light_cyan', attrs=['bold'])
             wait_for_keypress()
+
+            user_menu()
+            return
+
+
+def print_main_menu():
+    cprint(f'\n\n{"Menu":^16}', 'white', attrs=['bold'])
+    print('-'*16)
+    print('  (L)og In')
+    cprint('  (Q)uit\n\n', 'red')
+
+
+def print_user_menu():
+    clear()
+    cprint(f'\n\n{"User Menu":^26}', 'white', attrs=['bold'])
+    print('-'*26)
+    print('  (U)ser Info')
+    print('  (A)ssessment History')
+    print('  (C)ompetencies')
+    cprint('  (L)og Out\n\n', 'red')
+
+
+def user_menu():
+    global current_user
+    while True:
+        print_user_menu()
+        inputs = {'U': current_user.print_info,
+                  'A': 'assessment history',
+                  'C': 'competencies'}
+        
+        user_input = input().upper()
+        clear()
+
+        if user_input == 'L':
+            query = find_query('Update Status')
+            cursor.execute(query, (0, current_user.user_id))
+            connection.commit()
+            return
+        
+        if user_input in inputs.keys():
+            inputs[user_input]()
+        
+        else:
+            cprint('\n\nInvalid input. Try again.', 'red')
+            wait_for_keypress()
+            continue
 
 
 
@@ -402,13 +625,14 @@ def main():
         
         else:
             cprint('\n\nInvalid input. Try again.', 'red')
+            wait_for_keypress()
             continue
 
 
 if __name__ == '__main__':
+    create_database()
     database = 'test.db'
     connection = sqlite3.connect(database)
     cursor = connection.cursor()
-    create_database()
 
     main()
